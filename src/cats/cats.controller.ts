c81cd0449d6b258205b1c4c3e9bb203dcfa5b15d
OusmaneMballo <ousmanemballo576@gmail.com>
2021-02-02 02:42:28 +0100

    Gestion des routes

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, HttpCode, Post } from '@nestjs/common';
+import { Controller, Get, HttpCode, Param, Post, Query, Redirect } from '@nestjs/common';
+import { version } from 'prettier';
 
 @Controller('cats')
 export class CatsController {
@@ -20,4 +21,25 @@ export class CatsController {
     findAll(): string{
         return "Cette fonction retourne la liste des chats"
     }
+
+    @Get('docs')
+    @Redirect('https://docs.nestjs.com',302)
+    getdocs(@Query('version') version){
+
+        if (version && version === '5') {
+            return { url: 'https://docs.nestjs.com/v5/' };
+          }
+        return ""
+    }
+
+    @Get(':id')
+    findById(@Param() params): string{
+        return `This action returns a #${params.id} cat`;
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string): string {
+        return `This action returns a #${id} cat`;
+    }
+
 }
