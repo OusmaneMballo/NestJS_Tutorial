3e0fc510cb31760c0d8779c25ad8a6a609edee7f
Ousmane MBALLO <ousmane.mballo@fahpo.com>
2021-02-23 02:25:42 +0100

    Creation du module dotos

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
+import { TodosModule } from './todos/todos.module';
 
 @Module({
-  imports: [],
+  imports: [TodosModule],
   controllers: [AppController, CatsController],
   providers: [AppService],
 })
