8b45b4fe78db9dfeff5e28fc2d04dea56875c0f4
Ousmane MBALLO <ousmane.mballo@fahpo.com>
2021-02-23 03:07:24 +0100

    Creation de l'interface Todo

---
+++ src/interfaces/todo.interface.ts
@@ -0,0 +1,8 @@
+import { Interface } from "readline";
+
+export interface Todo{
+    id: number,
+    title: String,
+    description: String,
+    done: boolean
+}
--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { Todo } from 'src/interfaces/todo.interface';
 
 @Injectable()
 export class TodosService {
@@ -6,21 +7,24 @@ export class TodosService {
         {
             'id': 1,
             'title': 'Todos app',
-            'description': 'Create Nest Todo App'
+            'description': 'Create Nest Todo App',
+            'done': true
         },
         {
             'id': 2,
             'title': 'Bread',
-            'description': 'Acheter du pain'
+            'description': 'Acheter du pain',
+            'done': true
         },
         {
             'id': 3,
             'title': 'Menage',
-            'description': 'Ranger la Chambre'
+            'description': 'Ranger la Chambre',
+            'done': false
         },
     ];
 
-    findAll(): any[]{
+    findAll(): Todo[]{
         return this.todos
     }
 }
