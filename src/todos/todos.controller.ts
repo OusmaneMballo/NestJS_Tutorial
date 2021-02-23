91a2e5bbefd4c63dafc9e83ddd56ca3f3d3bd06b
Ousmane MBALLO <ousmane.mballo@fahpo.com>
2021-02-23 02:33:32 +0100

    generation du controller dotos

---
+++ src/todos/todos.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get } from '@nestjs/common';
+
+//localhost:3000/todos
+@Controller('todos')
+export class TodosController {
+
+    @Get()
+    findAll(): Promise<any[]>{
+        return 
+    }
+}
