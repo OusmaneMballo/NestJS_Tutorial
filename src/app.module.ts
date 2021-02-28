9ba20630f190c9690e41c6c4fe552146d9dca8ce
Ousmane MBALLO <ousmane.mballo@fahpo.com>
2021-02-28 03:37:12 +0100

    Creation du module Database

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
 import { TodosModule } from './todos/todos.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [TodosModule],
+  imports: [
+    TodosModule, 
+    DatabaseModule,
+  ],
   controllers: [AppController, CatsController],
   providers: [AppService],
 })
